f2b3a664678e08e479b5698f81d797ca0a0dae36
Dmitriy <h0rdes2010@gmail.com>
2020-03-15 21:23:04 +0500

    correction deleteResume in SortedArrayStorage (HW3)

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -11,11 +11,10 @@ public class ArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void deleteResume(int index) {
-        storage[index]=storage[size-1];
+        storage[index] = storage[size - 1];
     }
 
     protected int getIndex(String uuid) {
-
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
                 return i;
--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -8,14 +8,14 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void addResume(Resume resume, int index) {
-        int i = (-index - 1);
+        int i = -index - 1;
         System.arraycopy(storage, i, storage, i + 1, size - i);
         storage[i] = resume;
     }
 
     @Override
     protected void deleteResume(int index) {
-        System.arraycopy(storage, index + 1, storage, index, size - index);
+        System.arraycopy(storage, index + 1, storage, index, size - (index + 1));
     }
 
     @Override
