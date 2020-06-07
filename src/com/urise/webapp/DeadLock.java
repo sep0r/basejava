eec975b9bcbc12b5fc9a79200ad46bda087f575d
sep0r <h0rdes2010@gmail.com>
2020-06-07 13:02:20 +0500

    correction HW11

--- src/com/urise/webapp/DeadLock.java
+++ src/com/urise/webapp/DeadLock.java
@@ -1,61 +1,34 @@
 package com.urise.webapp;
 
 public class DeadLock {
-    static int counter;
+    static Object lock1 = new Object();
+    static Object lock2 = new Object();
 
     public static void main(String[] args) throws InterruptedException {
-        Runner runner = new Runner();
+        deadLock(lock1, lock2);
+        deadLock(lock2, lock1);
 
+    }
+
+    static void deadLock(Object lock1, Object lock2) throws InterruptedException {
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                runner.Thread1();
+                for (int i = 0; i < 10; i++) {
+                    synchronized (lock1) {
+                        try {
+                            System.out.println("took the lock1");
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        synchronized (lock2) {
+                            System.out.println("took the lock2");
+                        }
+                    }
+                }
             }
         });
-
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                runner.Thread2();
-            }
-        });
-
         thread1.start();
-        thread2.start();
-
-        thread1.join();
-        thread2.join();
-
-        System.out.println(counter);
-    }
-
-    public static void count() {
-        counter++;
-    }
-}
-
-class Runner {
-    DeadLock deadLock = new DeadLock();
-    Object lock1 = new Object();
-    Object lock2 = new Object();
-
-    public void Thread1() {
-        for (int i = 0; i < 10000; i++) {
-            synchronized (lock1) {
-                synchronized (lock2) {
-                    DeadLock.count();
-                }
-            }
-        }
-    }
-
-    public void Thread2() {
-        for (int i = 0; i < 10000; i++) {
-            synchronized (lock2) {
-                synchronized (lock1) {
-                    DeadLock.count();
-                }
-            }
-        }
     }
 }
